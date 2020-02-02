08db9a104d595c532ebb1bb46661ed19820c0551
Vanessa McHale <vamchale@gmail.com>
2020-02-01 19:11:09 -0600

    Use compressFile

--- mem/Mem.hs
+++ mem/Mem.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import           Codec.Lzip           (compress, decompress)
+import           Codec.Lzip           (compressFile, decompress)
 import qualified Data.ByteString.Lazy as BSL
 import           System.FilePath      ((</>))
 import           System.IO.Temp       (withSystemTempDirectory)
@@ -18,4 +18,4 @@ decompressDump = withSystemTempDirectory "lz" $
 compressDump :: IO ()
 compressDump = withSystemTempDirectory "lz" $
     \fp -> BSL.writeFile (fp </> "gmp-6.1.2.tar.lz") =<<
-        (compress <$> BSL.readFile "gmp-6.1.2.tar")
+        (compressFile "gmp-6.1.2.tar")
