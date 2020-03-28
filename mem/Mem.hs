597a361ee6b932fda0f8f4fbb5e251da52a75fdf
Vanessa McHale <vamchale@gmail.com>
2020-03-28 13:41:49 -0500

    in case we want threadscope?

--- mem/Mem.hs
+++ mem/Mem.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
 import           Codec.Lzip           (compressFile, decompress)
+import           Control.Concurrent   (forkIO, newEmptyMVar, putMVar, takeMVar)
+import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as BSL
 import           System.FilePath      ((</>))
 import           System.IO.Temp       (withSystemTempDirectory)
-
 main :: IO ()
 main =
     compressDump *>
-    decompressDump
+    decompressDump *>
+    decompressMultithreaded "gmp-6.1.2.tar.lz"
 
 decompressDump :: IO ()
 decompressDump = withSystemTempDirectory "lz" $
@@ -19,3 +21,17 @@ compressDump :: IO ()
 compressDump = withSystemTempDirectory "lz" $
     \fp -> BSL.writeFile (fp </> "gmp-6.1.2.tar.lz") =<<
         (compressFile "gmp-6.1.2.tar")
+
+forceHead :: BSL.ByteString -> IO ()
+forceHead bsl = BS.length (head $ BSL.toChunks bsl) `seq` mempty
+
+forceBSL :: BSL.ByteString -> IO ()
+forceBSL bsl = BS.length (last $ BSL.toChunks bsl) `seq` mempty
+
+decompressMultithreaded :: FilePath -> IO ()
+decompressMultithreaded fp = do
+    bsl <- decompress <$> BSL.readFile fp
+    forceHead bsl
+    done <- newEmptyMVar
+    _ <- forkIO (forceBSL bsl *> putMVar done ())
+    takeMVar done
