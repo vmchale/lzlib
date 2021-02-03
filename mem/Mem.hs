d60e1d715ded696003048855f6e762a0e1c2360b
Vanessa McHale <vanessa.mchale@bitnomial.com>
2021-02-03 14:21:16 -0600

    paren

--- mem/Mem.hs
+++ mem/Mem.hs
@@ -14,13 +14,12 @@ main =
 
 decompressDump :: IO ()
 decompressDump = withSystemTempDirectory "lz" $
-    \fp -> BSL.writeFile (fp </> "gmp-6.1.2.tar") =<<
-        (decompress <$> BSL.readFile "gmp-6.1.2.tar.lz")
+    \fp -> BSL.writeFile (fp </> "gmp-6.1.2.tar") . decompress =<< BSL.readFile "gmp-6.1.2.tar.lz"
 
 compressDump :: IO ()
 compressDump = withSystemTempDirectory "lz" $
     \fp -> BSL.writeFile (fp </> "gmp-6.1.2.tar.lz") =<<
-        (compressFile "gmp-6.1.2.tar")
+        compressFile "gmp-6.1.2.tar"
 
 forceHead :: BSL.ByteString -> IO ()
 forceHead bsl = BS.length (head $ BSL.toChunks bsl) `seq` mempty
